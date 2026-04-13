087dd646b77b1cc39ee419191aaeae02b4e2c8cf
[spring]qwe3364 <qwe3364@gmail.com>
2026-04-13 11:43:39 +0900

    ....

---
+++ kr/story/setup-guide/js/styler/style2.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    LGE_CONFIGURATION_OPTION.initialize("Styler");
+
+    // Styler 전용:
+    const elStyler = document.querySelector(".test---Styler");
+    if (elStyler) {
+    }
+});
